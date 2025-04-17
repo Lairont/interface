428f23cc2341090c004498fb4b20e3021e412a3f
Никита <153456515+Lairont@users.noreply.github.com>
2025-04-17 10:26:29 +0300

    Add files via upload

---
+++ ConsoleApp9/DerivedClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class DerivedClass : Iinrerface //Реализует интерфейс
+    {
+        public void Method()
+        {
+            Console.WriteLine("Метод Method,  реал-й в классе DerivedClass");
+        }
+    }
+}
---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass derivedClass = new DerivedClass();
+            derivedClass.Method();
+            Console.WriteLine();
+            Iinrerface myInrerface = derivedClass;
+            myInrerface.Method();
+            Console.Read();
+        }
+    }
+}

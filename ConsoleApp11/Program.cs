c5f0948e78add2ebe5a392587c19529af2d382d2
Никита <153456515+Lairont@users.noreply.github.com>
2025-04-17 11:06:14 +0300

    Add files via upload

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass derivedClass = new DerivedClass();
+            derivedClass.Method1();
+            derivedClass.Method2();
+            derivedClass.Method3();
+            Console.WriteLine();
+            IInterface2 dClass = derivedClass;
+            derivedClass.Method1();
+            derivedClass.Method2();
+            derivedClass.Method3();
+            Console.Read();
+        }
+    }
+}
